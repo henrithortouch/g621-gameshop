3d74b126a95968882c352f9bdd75f0c1ec7b40a7
Thor-Touch Henri <henri.thor-touch@aalto.fi>
2018-01-09 14:47:32 +0200

    Landing page functionality

--- gameshop/views.py
+++ gameshop/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
+from django.template import loader
 
 def about(request):
 	return HttpResponse("about page")
 
+def home(request):
+	template = loader.get_template("gameshop/home.html")
+	output = template.render(context)
+	return HttpResponse(output)
+	
 # Create your views here.
