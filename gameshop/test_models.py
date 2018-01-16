72cc152c9f70b397cbfb9d9d556511e75130424f
Simo Muraja <simo.muraja@aalto.fi>
2018-01-16 15:47:28 +0200

    Extend test

--- gameshop/test_models.py
+++ gameshop/test_models.py
@@ -7,18 +7,25 @@ class GameTests(TestCase):
     def setUp(self):
         pass
 
-    def test_add_game_adds_game(self):
-        g = Game(name = "töhö", description = "lul")
-        u = User()
-        g.save()
-        u.save()
-        u.addGame(g)
+    def test_add_game_existing_games_both_exist(self):
+        g1 = Game(name = "1", description = "")
+        g2 = Game(name = "2", description = "")
+        g3 = Game(name = "3", description = "")
+        g1.save()
+        g2.save()
+        g3.save()
+
+        u = self.gen_user()
+        u.addGame(g1)
+        u.addGame(g2)
 
         result = u.games_bought
-        self.assertEqual(result.values().first()["name"], g.name)
-        self.assertEqual(result.count(), 1)
+        self.assertTrue(result.filter(name = "1"))
+        self.assertTrue(result.filter(name = "2"))
+        self.assertFalse(result.filter(name = "3"))
+        self.assertEqual(result.count(), 2)
 
-    
-
-    def gen_user():
-        return User(user_name="")
+    def gen_user(self):
+        u = User(user_name="")
+        u.save()
+        return u
