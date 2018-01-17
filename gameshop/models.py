3959953bee907fda9db7906ff8e3a5c63ecca2ed
Simo Muraja <simo.muraja@aalto.fi>
2018-01-17 17:42:44 +0200

    Dev models. Much WIP

--- gameshop/models.py
+++ gameshop/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# Create your models here.
 class Game(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True)
     name = models.CharField(max_length=50)
@@ -15,8 +14,8 @@ class Game(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    is_dev = models.BooleanField(default=False)
     games_bought = models.ManyToManyField(Game)
+    games_owned = models.ManyToManyField(Game, through='Ownership')
 
     def addGame(self, game):
         self.games_bought.add(game)
@@ -25,6 +24,11 @@ class Profile(models.Model):
     def __str__(self):
         return "\nUsername: " +self.user.username + "\n" \
             + "Is dev: %s" % self.is_dev
+
+class Ownership(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    sales = models.IntegerField(default=0)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
