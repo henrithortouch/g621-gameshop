b89cc453849a012383827a288058c49cb45349bf
Kumkvatti <hansen.feng50@gmail.com>
2018-02-14 17:24:27 +0200

    Add gamescreen setting request handler

--- gameshop/static/gameshop/gamescreen.js
+++ gameshop/static/gameshop/gamescreen.js
@@ -4,8 +4,26 @@
 
             window.addEventListener("message", function(evt) {
                 if(evt.data.messageType === "LOAD_REQUEST") {
+                    var response = $.ajax({
+                        type: "GET",
+                        beforeSend: function(request) {
+                            request.setRequestHeader("X-CSRFToken", document.getElementsByName('csrfmiddlewaretoken')[0].value);
+                        },
+                        url: window.location + "load/",
+                        success: function(data, status, xhttp) {
+                            console.log(status)
+                            if (status === "success"){
+                                console.log(data)
+                                var obj = JSON.parse(data)
+                                var message = {
+                                    messageType: "SAVE",
+                                    gameState: obj,
+                                }
+                                console.log(obj)
+                            }
+                        },
+                    });
                     console.log("LOAD")
-
                 } else if(evt.data.messageType === "SAVE") {
                     var msg = evt.data.gameState
                     console.log(msg)
@@ -17,6 +35,7 @@
                         url: window.location + "save/",
                         data: msg,
                         success: function(data, status, xhttp) {
+                            //TODO: Send error to iframe if not success
                             console.log("SUCCESS")
                         },
                     });
@@ -33,18 +52,21 @@
                         url: window.location + "score/",
                         data: {score: score},
                         success: function(data, status, xhttp) {
-                            if (status === 200){
+                            if (status === "success"){
                                 console.log("SUCCESS")
                             } else {
+                                //TODO: Send error to iframe
                                 console.log(data)
                                 console.log("FAILURE")
                             }
-                            
                         },
                     });
                     // Saves score to highscores
                     console.log("SCORE")
-                }
+                } else if(evt.data.messageType === "SETTING"){
+                    $("#game_frame").attr("width", evt.data.options.width)
+                    $("#game_frame").attr("height", evt.data.options.height)
+                } // else do nothing, maybe send a error to iframe?
             });
 
             // simple function to post to iframe, doesn't really do anything yet
