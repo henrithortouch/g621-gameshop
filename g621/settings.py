6c50682cac172cc6d8a3c9f7f24f68687be5ad93
Henri <loran.duong@hotmail.com>
2018-02-16 18:45:42 +0200

    Test local db

--- g621/settings.py
+++ g621/settings.py
@@ -84,7 +84,10 @@ WSGI_APPLICATION = 'g621.wsgi.application'
 #
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=500)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
