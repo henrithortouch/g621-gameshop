c3c5dbf9291daa9d68c4c313876282f288bd799e
Simo Muraja <simo.muraja@aalto.fi>
2018-02-24 23:32:37 +0200

    Remove obsolete test

--- gameshop/test_models.py
+++ gameshop/test_models.py
@@ -8,18 +8,6 @@ class GameTests(TestCase):
     def setUp(self):
         pass
 
-    def test_addGame_(self):
-        dev = self.generate_developer()
-        g1 = self.generate_game("1", dev)
-        g2 = self.generate_game("2", dev)
-
-        dev.profile.addGame(g1)
-
-        result = dev.profile.gamesBought()
-        self.assertTrue(result.filter(name = "1"))
-        self.assertFalse(result.filter(name = "2"))
-        self.assertEqual(result.count(), 1)
-
     def generate_profile(self):
         user = User.objects.create_user("name", "mail", "password")
         return user.profile
